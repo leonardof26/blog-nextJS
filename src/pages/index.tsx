0911910e592cedb9a9581a5badb307051ce2ada6
leonardopf26 <leonardopf26@gmail.com>
2021-04-24 11:05:48 -0300

    Added logic to get posts from prismic to list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,11 @@ import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+import { format, parseISO } from 'date-fns'
+import pt from 'date-fns/locale/pt-BR'
+
 import { FiCalendar, FiUser, FiClock } from 'react-icons/fi'
 
 import { getPrismicClient } from '../services/prismic'
@@ -28,7 +33,9 @@ interface HomeProps {
   postsPagination: PostPagination
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const { next_page: nextPage, results: posts } = postsPagination
+
   return (
     <>
       <Head>
@@ -37,49 +44,69 @@ export default function Home(): JSX.Element {
 
       <main className={`${commonStyles.content}`}>
         <div className={styles.posts}>
-          <Link href="/">
-            <a>
-              <strong>Como Utilizar Hooks</strong>
-              <p>jnsadnjksadnjksan sjadnaskjd sjadsajd jasdnjksad</p>
-              <span>
-                <time>
-                  <FiCalendar />
-                  15 mar 2021
-                </time>
-
+          {posts.map(post => (
+            <Link href="/" key={post.uid}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
                 <span>
-                  <FiUser />
-                  Leonardo Fontes
-                </span>
-              </span>
-            </a>
-          </Link>
-          <Link href="/">
-            <a>
-              <strong>Como Utilizar Hooks</strong>
-              <p>jnsadnjksadnjksan sjadnaskjd sjadsajd jasdnjksad</p>
-              <span>
-                <time>
-                  <FiCalendar />
-                  15 mar 2021
-                </time>
+                  <time>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </time>
 
-                <span>
-                  <FiUser />
-                  Leonardo Fontes
+                  <span>
+                    <FiUser />
+                    {post.data.author}
+                  </span>
                 </span>
-              </span>
-            </a>
-          </Link>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   )
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: [
+        'publication.title',
+        'publication.subtitle',
+        'publication.author',
+      ],
+      pageSize: 100,
+    }
+  )
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => ({
+    uid: post.uid,
+    first_publication_date: format(
+      parseISO(post.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: pt,
+      }
+    ),
+
+    data: {
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+
+      author: post.data.author,
+    },
+  }))
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  }
+
+  return {
+    props: { postsPagination },
+  }
+}
